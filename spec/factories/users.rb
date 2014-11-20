33554201a7df13c923ef027ca6ebdc5e35078673
Raja Hafify <rajahafify.zaed@gmail.com>
2014-11-20 16:03:08 +0800

    Added validations

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-
+    sequence(:email) { |n| "email+#{n}{Time.now}@email.com" }
+    password 'password'
+    username 'username'
+    gender 'Male'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,31 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
   let(:object) { build :user }
   subject { object }
-  it { is_expected.to respond_to :username }
   it { is_expected.to respond_to :email }
   it { is_expected.to respond_to :password }
+
+  it { is_expected.to respond_to :username }
+
   it { is_expected.to respond_to :profile_photo }
+
   it { is_expected.to respond_to :location }
+
   it { is_expected.to respond_to :gender }
+
+  describe 'validations' do
+    context 'invalid' do
+      it "username not unique" do
+        new_user = create :user, username: 'Name'
+        subject.username = 'Name'
+        expect(subject).not_to be_valid
+      end
+
+      it "using invalid gender" do
+        ['invalid', 'Gender'].each do |invalid|
+          subject.gender = invalid
+          expect(subject).not_to be_valid
+        end
+      end
+    end
+  end
 end
