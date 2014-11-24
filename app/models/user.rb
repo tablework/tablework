db1865744a1304ab99bfe61d97355977d8bfe358
fylooi <fy.looi@gmail.com>
2014-11-24 13:07:24 +0800

    adds facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,26 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
   validates :username, uniqueness: true
 
   validate :valid_gender
   before_validation { self.gender.downcase! if self.gender }
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]    
+      #user.name = auth.info.name   # assuming the user model has a name
+      #user.image = auth.info.image # assuming the user model has an image
+    end
+  end
 
   private
 
   def valid_gender
-    unless self.gender == 'male' or self.gender == 'female'
+    unless self.gender == 'male' or self.gender == 'female' or self.gender == nil
       errors.add(:gender, "must be valid.")
     end
   end
