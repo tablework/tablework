f425f5e4377d9acb01581330d83ffe4689d13481
Raja Hafify <rajahafify.zaed@gmail.com>
2014-11-26 12:10:19 +0800

    Added characters

---
+++ db/migrate/20141126034605_create_characters.rb
@@ -0,0 +1,15 @@
+class CreateCharacters < ActiveRecord::Migration
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.text :description
+      t.string :type_of_play
+      t.integer :age
+      t.string :occupation
+      t.hstore :fields, default: {}
+      t.belongs_to :user, index: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/characters.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :character do
+    name "MyString"
+    description "MyText"
+    type_of_play "MyString"
+    age 1
+    occupation "MyString"
+    fields "{}"
+    user nil
+  end
+
+end
