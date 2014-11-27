219b79d81312b07ed7f7febd00b084bcdf1ae40f
fylooi <fy.looi@gmail.com>
2014-11-27 16:29:24 +0800

    adds nested routes for characters

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  resources :characters, only: [:index, :new]
+  get 'profiles/index'
+
+  resources :characters do
+    resources :notes, only: [:create, :destroy]  
+    resources :scenes do
+      resources :questions, only: [:create, :destroy]
+    end
+  end
+
+  get "profile" => "profiles#index", as: :profile
 
   devise_for :users, :controllers => { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   root 'application#index'
 end
