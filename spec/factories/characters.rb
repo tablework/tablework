d3187510e7660e27d53285071b3b3a81d8311a22
Raja Hafify <rajahafify.zaed@gmail.com>
2014-12-17 12:06:10 +0800

    Added annotate

--- spec/factories/characters.rb
+++ spec/factories/characters.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: characters
+#
+#  id           :integer          not null, primary key
+#  name         :string(255)
+#  description  :text
+#  type_of_play :string(255)
+#  age          :integer
+#  occupation   :string(255)
+#  fields       :hstore           default({})
+#  user_id      :integer
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 FactoryGirl.define do
   factory :character do
     name "MyString"
