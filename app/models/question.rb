c0da4898061bfe6e9c647df941b20766a3e1e3ca
Tablework <Razif@tablework.com>
2014-12-17 12:06:10 +0800

    Added annotate

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id                :integer          not null, primary key
+#  title             :string(255)
+#  answer            :string(255)
+#  questionable_id   :integer
+#  questionable_type :string(255)
+#  created_at        :datetime
+#  updated_at        :datetime
+#
+
 class Question < ActiveRecord::Base
   belongs_to :questionable, :polymorphic => true
 end
