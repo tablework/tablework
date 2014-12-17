8af92e49cd3fb2d5762d33b43abfd1b369d518be
Tablework <Razif@tablework.com>
2014-12-17 14:23:56 +0800

    Get spec to work later

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,22 +5,12 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
-
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+  config.include Features::SessionHelpers, type: :controller
   config.include Features::SessionHelpers, type: :feature
   config.include Capybara::DSL
   config.include Devise::TestHelpers, :type => :controller
