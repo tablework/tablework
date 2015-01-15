96040f0765d6e44b242b5ab236135e011945f8fa
Tablework <Razif@tablework.com>
2015-01-16 00:05:10 +0800

    Updated scenable

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -1,18 +1,18 @@
 class ScenesController < ApplicationController
-  before_action :find_space, only: [:new, :create]
+  before_action :find_scenable, only: [:new, :create]
   def index
 
   end
 
   def new
-    @scene = @space.scenes.build
+    @scene = @scenable.scenes.build
   end
 
   def create
-    @scene = @space.scenes.build(scene_params)
+    @scene = @scenable.scenes.build(scene_params)
     if @scene.save
       flash[:notice] = 'Scene created'
-      redirect_to @space
+      redirect_to @scenable
     else
       flash.now[:error] = 'Scene creation fails'
       render :new
@@ -27,16 +27,11 @@ class ScenesController < ApplicationController
     )
   end
 
-  def find_space
-    @space = Space.find(params[:space_id])
-  end
-
   def find_scenable
     params.each do |name, value|
       if name =~ /(.+)_id$/
-        return $1.classify.constantize.find(value)
+        @scenable = $1.classify.constantize.find(value)
       end
     end
-    nil
   end
 end
