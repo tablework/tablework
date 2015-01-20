ff6621ab48bb8f68b1b894e3cd1e2a748b73fc69
kaizenx <a@a.com>
2015-01-20 18:34:56 +0800

    deleted the old portrait helper, and created a new combined portrait helper

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150120092610) do
-=======
-ActiveRecord::Schema.define(version: 20150120015950) do
->>>>>>> a626900a273f19e8a437bcc6d0f4c56c7bb22f06
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
