4f27ff697187a7507f9e669439006f899c1f554c
Tablework <Razif@tablework.com>
2015-01-21 15:39:56 +0800

    Annotate authorization

--- spec/factories/authorizations.rb
+++ spec/factories/authorizations.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: authorizations
+#
+#  id         :integer          not null, primary key
+#  provider   :string(255)
+#  uid        :string(255)
+#  user_id    :integer
+#  token      :string(255)
+#  secret     :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#  username   :string(255)
+#
+
 FactoryGirl.define do
   factory :authorization do
     provider "Facebook"
