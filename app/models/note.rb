02142893e2491b69a698ba8d1dc2ad1c208129bb
Tablework <Razif@tablework.com>
2015-01-27 04:13:38 +0800

    Finished with space note

--- app/models/note.rb
+++ app/models/note.rb
@@ -17,4 +17,20 @@
 class Note < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   belongs_to :notable, polymorphic: true
+
+  before_save :clean_link_html
+
+  auto_html_for :link do
+    html_escape
+    image
+    youtube(:width => 235, :height => 180, :autoplay => false)
+    link :target => "_blank", :rel => "nofollow"
+    simple_format
+  end
+
+  private
+
+  def clean_link_html
+    self.link_html = nil if self.link_html == "<p><br></p>"
+  end
 end
