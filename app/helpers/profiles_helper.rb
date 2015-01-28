def7e819e3380f629de4694f61d8df6f7cee021d
kaizenx <a@a.com>
2015-01-28 18:15:39 +0800

    changed the profile editing modal popup to look sleeker

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,7 +1,14 @@
 module ProfilesHelper
 	def ImagePreviewer(user)
-		if user.image
-			image_tag(user.image, class: 'profile-editor-preview')
+		imageUrl = 'portrait.jpg'
+		#{Rails.root}/tmp/uploads
+		if user.image.url
+			imageUrl = user.image
+		elsif user.profile_photo
+			imageUrl = user.profile_photo
 		end
+		image_tag(imageUrl, class: 'profile-editor-preview')
 	end
 end
+
+
