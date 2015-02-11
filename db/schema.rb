a66f74ea759e1a6f334bfc872759f6c34f0d7211
kaizenx <a@a.com>
2015-02-11 18:42:07 +0800

    worked on paypal, recurring payment still an issue

--- db/schema.rb
+++ db/schema.rb
@@ -18,29 +18,30 @@ ActiveRecord::Schema.define(version: 20150202043900) do
   enable_extension "hstore"
 
   create_table "authorizations", force: :cascade do |t|
-    t.string   "provider"
-    t.string   "uid"
+    t.string   "provider",   limit: 255
+    t.string   "uid",        limit: 255
     t.integer  "user_id"
-    t.string   "token"
-    t.string   "secret"
+    t.string   "token",      limit: 255
+    t.string   "secret",     limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "username"
+    t.string   "username",   limit: 255
   end
 
   create_table "characters", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",         limit: 255
     t.text     "description"
-    t.string   "type_of_play"
+    t.string   "type_of_play", limit: 255
     t.integer  "age"
-    t.string   "occupation"
+    t.string   "occupation",   limit: 255
+    t.hstore   "fields",                   default: {}
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "nationality"
+    t.string   "nationality",  limit: 255
     t.datetime "DOB"
     t.integer  "space_id"
-    t.string   "gender"
+    t.string   "gender",       limit: 255
     t.string   "image"
   end
 
@@ -48,11 +49,11 @@ ActiveRecord::Schema.define(version: 20150202043900) do
   add_index "characters", ["user_id"], name: "index_characters_on_user_id", using: :btree
 
   create_table "notes", force: :cascade do |t|
-    t.string   "title"
+    t.string   "title",        limit: 255
     t.integer  "notable_id"
-    t.string   "notable_type"
+    t.string   "notable_type", limit: 255
     t.text     "text"
-    t.string   "image"
+    t.string   "image",        limit: 255
     t.text     "link"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -62,10 +63,10 @@ ActiveRecord::Schema.define(version: 20150202043900) do
   add_index "notes", ["notable_id"], name: "index_notes_on_notable_id", using: :btree
 
   create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.string   "answer"
+    t.string   "title",             limit: 255
+    t.string   "answer",            limit: 255
     t.integer  "questionable_id"
-    t.string   "questionable_type"
+    t.string   "questionable_type", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -84,9 +85,9 @@ ActiveRecord::Schema.define(version: 20150202043900) do
   add_index "scene_memberships", ["user_id"], name: "index_scene_memberships_on_user_id", using: :btree
 
   create_table "scenes", force: :cascade do |t|
-    t.string   "title"
+    t.string   "title",         limit: 255
     t.integer  "scenable_id"
-    t.string   "scenable_type"
+    t.string   "scenable_type", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -98,8 +99,8 @@ ActiveRecord::Schema.define(version: 20150202043900) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email"
-    t.string   "token"
+    t.string   "email",      limit: 255
+    t.string   "token",      limit: 255
   end
 
   add_index "space_memberships", ["email"], name: "index_space_memberships_on_email", using: :btree
@@ -108,9 +109,9 @@ ActiveRecord::Schema.define(version: 20150202043900) do
   add_index "space_memberships", ["user_id"], name: "index_space_memberships_on_user_id", using: :btree
 
   create_table "spaces", force: :cascade do |t|
-    t.string   "name"
-    t.string   "type_of_play"
-    t.string   "description"
+    t.string   "name",         limit: 255
+    t.string   "type_of_play", limit: 255
+    t.string   "description",  limit: 255
     t.integer  "director_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -119,33 +120,33 @@ ActiveRecord::Schema.define(version: 20150202043900) do
   add_index "spaces", ["director_id"], name: "index_spaces_on_director_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "username"
-    t.string   "profile_photo"
-    t.string   "location"
-    t.string   "gender"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "occupation"
-    t.string   "skillset"
-    t.string   "eyecolor"
+    t.string   "username",               limit: 255
+    t.string   "profile_photo",          limit: 255
+    t.string   "location",               limit: 255
+    t.string   "gender",                 limit: 255
+    t.string   "provider",               limit: 255
+    t.string   "uid",                    limit: 255
+    t.string   "occupation",             limit: 255
+    t.string   "skillset",               limit: 255
+    t.string   "eyecolor",               limit: 255
     t.date     "DOB"
-    t.string   "mobile_phone"
-    t.string   "website"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "image"
+    t.string   "mobile_phone",           limit: 255
+    t.string   "website",                limit: 255
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
+    t.string   "image",                  limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
