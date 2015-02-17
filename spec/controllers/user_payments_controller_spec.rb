0495c602980b67c87bc4b4e0578a9a122bffc434
kaizenx <a@a.com>
2015-02-17 15:43:50 +0800

    rspec

--- spec/controllers/user_payments_controller_spec.rb
+++ spec/controllers/user_payments_controller_spec.rb
@@ -23,136 +23,22 @@ RSpec.describe UserPaymentsController, :type => :controller do
   # This should return the minimal set of attributes required to create a valid
   # UserPayment. As you add validations to UserPayment, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # UserPaymentsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
-
-  describe "GET index" do
-    it "assigns all user_payments as @user_payments" do
-      user_payment = UserPayment.create! valid_attributes
-      get :index, {}, valid_session
-      expect(assigns(:user_payments)).to eq([user_payment])
-    end
+  before do
+    @space = create :space
+    request.env["HTTP_REFERER"] = space_path(@space)
+    sign_in :user, user
   end
 
-  describe "GET show" do
-    it "assigns the requested user_payment as @user_payment" do
-      user_payment = UserPayment.create! valid_attributes
-      get :show, {:id => user_payment.to_param}, valid_session
-      expect(assigns(:user_payment)).to eq(user_payment)
-    end
-  end
-
-  describe "GET new" do
-    it "assigns a new user_payment as @user_payment" do
-      get :new, {}, valid_session
-      expect(assigns(:user_payment)).to be_a_new(UserPayment)
-    end
-  end
-
-  describe "GET edit" do
-    it "assigns the requested user_payment as @user_payment" do
-      user_payment = UserPayment.create! valid_attributes
-      get :edit, {:id => user_payment.to_param}, valid_session
-      expect(assigns(:user_payment)).to eq(user_payment)
-    end
-  end
-
-  describe "POST create" do
-    describe "with valid params" do
-      it "creates a new UserPayment" do
-        expect {
-          post :create, {:user_payment => valid_attributes}, valid_session
-        }.to change(UserPayment, :count).by(1)
-      end
-
-      it "assigns a newly created user_payment as @user_payment" do
-        post :create, {:user_payment => valid_attributes}, valid_session
-        expect(assigns(:user_payment)).to be_a(UserPayment)
-        expect(assigns(:user_payment)).to be_persisted
-      end
-
-      it "redirects to the created user_payment" do
-        post :create, {:user_payment => valid_attributes}, valid_session
-        expect(response).to redirect_to(UserPayment.last)
-      end
+  describe 'GET #new' do
+    it "should be success" do
+      get :new, space_id: @space.id
+      expect(response).to be_success
     end
 
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved user_payment as @user_payment" do
-        post :create, {:user_payment => invalid_attributes}, valid_session
-        expect(assigns(:user_payment)).to be_a_new(UserPayment)
-      end
-
-      it "re-renders the 'new' template" do
-        post :create, {:user_payment => invalid_attributes}, valid_session
-        expect(response).to render_template("new")
-      end
-    end
-  end
-
-  describe "PUT update" do
-    describe "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested user_payment" do
-        user_payment = UserPayment.create! valid_attributes
-        put :update, {:id => user_payment.to_param, :user_payment => new_attributes}, valid_session
-        user_payment.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "assigns the requested user_payment as @user_payment" do
-        user_payment = UserPayment.create! valid_attributes
-        put :update, {:id => user_payment.to_param, :user_payment => valid_attributes}, valid_session
-        expect(assigns(:user_payment)).to eq(user_payment)
-      end
-
-      it "redirects to the user_payment" do
-        user_payment = UserPayment.create! valid_attributes
-        put :update, {:id => user_payment.to_param, :user_payment => valid_attributes}, valid_session
-        expect(response).to redirect_to(user_payment)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns the user_payment as @user_payment" do
-        user_payment = UserPayment.create! valid_attributes
-        put :update, {:id => user_payment.to_param, :user_payment => invalid_attributes}, valid_session
-        expect(assigns(:user_payment)).to eq(user_payment)
-      end
-
-      it "re-renders the 'edit' template" do
-        user_payment = UserPayment.create! valid_attributes
-        put :update, {:id => user_payment.to_param, :user_payment => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE destroy" do
-    it "destroys the requested user_payment" do
-      user_payment = UserPayment.create! valid_attributes
-      expect {
-        delete :destroy, {:id => user_payment.to_param}, valid_session
-      }.to change(UserPayment, :count).by(-1)
-    end
-
-    it "redirects to the user_payments list" do
-      user_payment = UserPayment.create! valid_attributes
-      delete :destroy, {:id => user_payment.to_param}, valid_session
-      expect(response).to redirect_to(user_payments_url)
+    it "should have correct assigns" do
+      get :new, space_id: @space.id
+      expect(assigns[:space_membership]).to be_instance_of(SpaceMembership)
+      expect(assigns[:space_membership]).to be_new_record
     end
   end
 
