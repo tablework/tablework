a58b9788a5e8ed98b022f1591203a28871c583ce
Raja Hafify <rajahafify.zaed@gmail.com>
2015-05-27 23:57:22 +0800

    Fix for character image getting compressed

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -2,8 +2,6 @@ module CharactersHelper
   def user_portrait options={style: 'width:100%; height: 100%'}
     if options[:user]
       user_use_uploaded_images(options)
-    elsif options[:gender] == 'male' or options[:gender] == 'female'
-      image_tag( options[:gender]+'.jpeg', style: options[:style], class: options[:class] )
     else
       image_tag("default-avatar-#{1 + rand(5)}.png", style: options[:style], class: options[:class] )
     end
@@ -16,6 +14,24 @@ module CharactersHelper
         image_tag options[:user].profile_photo, style: options[:style], class: options[:class]
       else
       image_tag("default-avatar-#{1 + rand(5)}.png", style: options[:style], class: options[:class] )
+    end
+  end
+
+  def user_portrait_url user
+    if user
+      user_use_uploaded_images_url user
+    else
+      "default-avatar-#{1 + rand(5)}.png"
+    end
+  end
+
+  def user_use_uploaded_images_url user
+    if user.image.present?
+      user.image.url
+    elsif user.try(:profile_photo)
+      user.profile_photo
+    else
+      "default-avatar-#{1 + rand(5)}.png"
     end
   end
 end
