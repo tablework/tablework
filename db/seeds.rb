6a362ca5aca4a9b7f2d2601c379c46aff3b6e61a
rajahafify <rajahafify.zaed@gmail.com>
2015-08-03 21:31:52 +0800

    Using subscription plans attr to determine user access

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 if SubscriptionPlan.all.empty?
-  SubscriptionPlan.create(name: 'Free', owned_space: false, character_limit: 2, state: 'active')
+  SubscriptionPlan.create(name: 'Free', owned_space: false, character_limit: 3, state: 'active')
   SubscriptionPlan.create(name: 'Pro', owned_space: true, character_limit: 1_000_000, state: 'active')
 end
