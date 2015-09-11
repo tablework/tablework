a882307f90398a16aee0817af6ed4d59b1e40632
rajahafify <rajahafify.zaed@gmail.com>
2015-09-11 23:30:03 +0800

    Add contact page

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,7 +55,7 @@ class User < ActiveRecord::Base
   has_many :spaces, through: :space_memberships
   has_many :user_payments
   has_many :messages, dependent: :destroy
-  has_one :subscription
+  has_one :subscription, dependent: :destroy
 
   after_create :set_sample_character
   after_create :set_sample_space
