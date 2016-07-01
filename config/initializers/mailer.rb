efc8944af704dd61841dd0c77318ed90d66a83ab
Raja Hafify <rajahafify.zaed@gmail.com>
2016-07-01 15:42:22 +0800

    Change mailgun domain

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -4,7 +4,7 @@ if Rails.env.production? || Rails.env.staging?
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'tablework.com',
+    :domain         => 'mg.tablework.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
